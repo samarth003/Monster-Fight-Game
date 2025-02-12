98b17845ac523542ff29df63019ef62ea6a90aa7
Samarth Kapoor <samarthkapoor@live.com>
2025-02-12 13:43:36 -0500

    This will update only the energy requirements to be used by Attack and defense methods

--- Source/Dragons.cpp
+++ Source/Dragons.cpp
@@ -4,71 +4,14 @@
 #include "Dragons.h"
 
 Dragons::Dragons(MonType type, const std::string& name)
-        : Monsters(type, name), m_BADmg {30}
-        ,m_CHADmg {100}, m_EnergyLeft {30} {}
+        : Monsters(type, name, BA_DMG, CHA_DMG, BASE_EN) 
+        {
 
-int Dragons::GetBaseHP() {return m_MonBaseHP;}
-int Dragons::GetMonHPLeft() {return m_MonHP; }        
-int Dragons::GetEnergyLeft() { return m_EnergyLeft; }
+        }
 
-bool Dragons::IsDefeated()
-{
-    if(m_MonHP <=0)
-        return true;
-    return false;
-}
-
-std::string& Dragons::GetMonName() { return m_MonsterName; }
-
-int Dragons::MonBasicAttack()
-{
-    if(m_EnergyLeft >= MIN_EN)
-        m_EnergyLeft -= BA_REQ_EN;
-    else
-    {
-        ErrorLog(ERR_NO_EN_BA);
-        return -1; 
-    }
-
-    return m_BADmg;    
-}
-
-int Dragons::MonChargedAttack()
-{
-    if(m_EnergyLeft >= CHA_REQ_EN)
-        m_EnergyLeft -= CHA_USED_EN;
-    else
-    {
-        ErrorLog(ERR_NO_EN_CHA); 
-        return -1;
-    }
-
-    return m_CHADmg;    
-}
-
-int Dragons::MonDefensiveShield()
-{
-    if(m_EnergyLeft < MAX_EN)
-    {
-        if((MAX_EN - m_EnergyLeft) >= REVIT_EN)
-            m_EnergyLeft += REVIT_EN;
-        else if((MAX_EN - m_EnergyLeft) < REVIT_EN)
-            m_EnergyLeft += (MAX_EN - m_EnergyLeft);
-    }
-    else
-    {
-        ErrorLog(ERR_MAX_EN);
-        return -1;
-    }
-    return 0;
-}
-
-void Dragons::MonDamageTaken(int DmgReceived)
-{   
-    if((m_MonHP - DmgReceived) <= 0)
-        m_MonHP = 0;
-    else
-        m_MonHP -= DmgReceived;
-    std::cout << m_MonsterName << " took " << DmgReceived << " damage" << std::endl;
-    std::cout << m_MonsterName << " has " << m_MonHP << " HP Left" << std::endl;
-}
+int Dragons::GetMinEnergyRequired() {return MIN_EN; }
+int Dragons::GetDissipatedEnergyBA() {return BA_REQ_EN; }
+int Dragons::GetRequiredCHAEnergy() {return CHA_REQ_EN; }
+int Dragons::GetEnergyUsedCHA() {return CHA_USED_EN; }
+int Dragons::GetMaxEnergy() {return MAX_EN; }
+int Dragons::GetRevitEnergy() {return REVIT_EN; }
