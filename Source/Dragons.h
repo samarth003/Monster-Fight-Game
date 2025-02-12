9c0079bf78aa2852073fa79e3d8cc4984ec3494d
Samarth Kapoor <samarthkapoor@live.com>
2025-02-12 13:45:06 -0500

    Decluttered the header file and made it more structured and a baseline to be used by other monsters classes

--- Source/Dragons.h
+++ Source/Dragons.h
@@ -3,9 +3,8 @@
 #include <iostream>
 
 #include "Monsters.h"
-#include "LogR.h"
 
-class Dragons : public Monsters, LogR
+class Dragons : public Monsters
 {
 public:
     Dragons(MonType type, const std::string& name);
@@ -13,48 +12,24 @@ public:
     enum Energy
     {
         MIN_EN = 15,                    //Minimum Energy required for any move to take place
-        BA_REQ_EN = 15,                 //Minimum Energy required for Basic attack 
+        BASE_EN = 30,
+        BA_REQ_EN = 15,                 //Minimum Energy required for Basic attack
         REVIT_EN = 25,                  //Energy to be revitalized when in defensive state
         CHA_USED_EN = 60,               //Energy used when using charged attack
         CHA_REQ_EN = 75,                //Energy required to use charged attack
         MAX_EN = 100                    //Maximum energy 
     };
 
-    int GetBaseHP() override;
-    int GetMonHPLeft() override;
-    int GetEnergyLeft();
-    bool IsDefeated() override;
-    std::string& GetMonName() override;
+    enum DmgStat
+    {
+        BA_DMG = 30,
+        CHA_DMG = 100
+    };
 
-    /**MOVESETS FOR DRAGON
-     * Basic Attack
-     * - DMG dealt 30
-     * - ENERGY USED 15 (minimum energy needed for BA)
-     */
-    int MonBasicAttack() override;
-
-     /* Charged Attack
-     * - DMG dealt 100
-     * - ENERGY needed 75 (Min energy needed for CHA)
-     * - ENERGY USED 60
-     */
-    int MonChargedAttack() override;
-
-     /* Defensive Shield
-     * - DMG dealt 0
-     * - ENERGY Revitalized 25 
-     */
-    int MonDefensiveShield() override;
-
-    /**Damage taken
-     * - Get DMG stats BA or CHA to calculate
-     * - left over health 
-     * - Returns none
-     */
-    void MonDamageTaken(int DmgReceived) override;
-    
-private:
-    const int m_BADmg {};
-    const int m_CHADmg {};
-    int m_EnergyLeft {};
+    int GetMinEnergyRequired() override;
+    int GetDissipatedEnergyBA() override;
+    int GetRequiredCHAEnergy() override;
+    int GetEnergyUsedCHA() override;
+    int GetMaxEnergy() override;
+    int GetRevitEnergy() override;
 };
