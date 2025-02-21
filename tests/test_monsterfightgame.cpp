75b8334dbc70dc292d4e29998332c69d56764f76
Samarth Kapoor <samarthkapoor@live.com>
2025-02-21 16:27:36 -0500

    Refactored test code

--- tests/test_monsterfightgame.cpp
+++ tests/test_monsterfightgame.cpp
@@ -8,195 +8,126 @@
 #include "MonsterFightGame.h"
 #include "Dragons.h"
 
-TEST(RosterCreation, RosterCreation_RosterSiz_Test)
+class MonsterFightGame : public ::testing::Test
+{
+protected:
+    std::vector<Monsters*> Roster;
+    std::string UserInput = " "; 
+    Monsters* Mon1 = nullptr; 
+    Monsters* Mon2 = nullptr; 
+    bool PlayBot = false;
+    const int m_MaxRetries = 3;
+    const int m_MaxRoster = 3; 
+
+    MonsterFightGame()
+    {
+
+    }
+
+    //Run before each test
+    void SetUp() override
+    {
+        Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "Silo"));
+        Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "Kaos")); 
+        Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "IronClaw"));         
+    }
+
+    //Run after each test
+    void TearDown() override
+    {
+        for(auto monster : Roster)
+            delete monster;
+    }
+
+    //Simulate monster selection return output to check for error
+    std::string SimulateMonSelection(const std::string& UserIn)
+    {
+        std::stringstream input(UserIn);
+        std::streambuf* orig_cin = std::cin.rdbuf(input.rdbuf()); 
+    
+        std::stringstream OutputBuf;
+        std::streambuf* orig_cout = std::cout.rdbuf(OutputBuf.rdbuf());
+    
+        MonSelection(Roster, Mon1, Mon2, PlayBot);
+    
+        std::cin.rdbuf(orig_cin); 
+        std::cout.rdbuf(orig_cout);
+    
+        return OutputBuf.str();
+    }
+
+};
+
+//Roster Creation
+TEST_F(MonsterFightGame, MonsterFightGame_RosterSize_Test)
 {
     std::vector<Monsters*> Roster;
     CreateRoster(Roster);
-
-    EXPECT_EQ(Roster.size(), 3); //Roster size check once roster is updated
-
-    for(auto monster : Roster)
-        delete monster;
+    EXPECT_EQ(Roster.size(), m_MaxRoster);
 }
 
-TEST(MonsterSelection, MonsterSelection_SelectMonsterNoBot_Test)
+//No Bot - valid selection
+TEST_F(MonsterFightGame, MonsterFightGame_SelectMonsterNoBot_Test)
 {
-    std::vector<Monsters*> Roster;
-    Monsters* Mon1 = nullptr; 
-    Monsters* Mon2 = nullptr;
-    bool PlayBot = false;
-
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "Silo"));
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "Kaos")); 
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "IronClaw")); 
-
+    UserInput = "N\n1\n3\n";
     //user input "N"- No bot, 1-Select Silo, 3-IronClaw 
-    std::stringstream input("N\n1\n3\n");
-    std::streambuf* orig_cin = std::cin.rdbuf(input.rdbuf()); 
-
-    MonSelection(Roster, Mon1, Mon2, PlayBot);
+    SimulateMonSelection(UserInput);
 
     EXPECT_EQ(Mon1->GetMonName(), "Silo");
     EXPECT_EQ(Mon2->GetMonName(), "IronClaw");
-    EXPECT_EQ(PlayBot, false);
-
-    std::cin.rdbuf(orig_cin);
-
-    for(auto monster : Roster)
-        delete monster;
+    EXPECT_FALSE(PlayBot);
 }
 
-TEST(MonsterSelection, MonsterSelection_SelectMonsterBot_Test)
+//Bot - valid selection
+TEST_F(MonsterFightGame, MonsterFightGame_SelectMonstersBot_Test)
 {
-    std::vector<Monsters*> Roster;
-    Monsters* Mon1 = nullptr; 
-    Monsters* Mon2 = nullptr;
-    bool PlayBot = false;
 
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "Silo"));
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "Kaos")); 
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "IronClaw")); 
-
+    UserInput = "Y\n1\n";
     //user input "Y"- bot, 1-Select Silo
-    std::stringstream input("Y\n1\n");
-    std::streambuf* orig_cin = std::cin.rdbuf(input.rdbuf()); 
-
-    MonSelection(Roster, Mon1, Mon2, PlayBot);
+    SimulateMonSelection(UserInput);
 
     EXPECT_EQ(Mon1->GetMonName(), "Silo");
-    EXPECT_EQ(PlayBot, true);
-
-    std::cin.rdbuf(orig_cin);
-
-    for(auto monster : Roster)
-        delete monster;
+    EXPECT_TRUE(PlayBot);
 }
 
-TEST(MonsterSelection, MonsterSelection_ExhaustRetries_Test)
+//No bot - exhaust all retries 
+TEST_F(MonsterFightGame, MonsterFightGame_ExhaustRetries_Test)
 {
-    std::vector<Monsters*> Roster;
-    Monsters* Mon1 = nullptr; 
-    Monsters* Mon2 = nullptr;
-    bool PlayBot = false;
 
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "Silo"));
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "Kaos")); 
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "IronClaw")); 
-
-    //user input "N"- No bot, 1-3 are valid inputs, rest invalid  
-    std::stringstream input("N\n1\n4\n3\n5\n2\n6\n");
-    std::streambuf* orig_cin = std::cin.rdbuf(input.rdbuf()); 
-
-    std::stringstream OutputBuf;
-    std::streambuf* orig_cout = std::cout.rdbuf(OutputBuf.rdbuf());
-
-    MonSelection(Roster, Mon1, Mon2, PlayBot);
-
-    std::cin.rdbuf(orig_cin); 
-    std::cout.rdbuf(orig_cout);
-
-    std::string RetString = OutputBuf.str();
-
+    UserInput = "N\n1\n4\n3\n5\n2\n6\n";
+    //user input "N"- No bot, 1-3 are valid inputs, rest invalid
+    std::string RetString = SimulateMonSelection(UserInput);  
     
     EXPECT_THAT(RetString, testing::HasSubstr("404"));  
-
-    for(auto monster : Roster)
-        delete monster;
 }
 
-TEST(MonsterSelection, MonsterSelection_ExhaustRetriesBot_Test)
+//Bot - Exhaust all retries
+TEST_F(MonsterFightGame, MonsterFightGame_ExhaustRetriesBot_Test)
 {
-    std::vector<Monsters*> Roster;
-    Monsters* Mon1 = nullptr; 
-    Monsters* Mon2 = nullptr;
-    bool PlayBot = false;
-
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "Silo"));
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "Kaos")); 
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "IronClaw")); 
-
-    //user input "Y"- bot, 1-3 are valid inputs, rest invalid  
-    std::stringstream input("Y\n5\n4\n7\n");
-    std::streambuf* orig_cin = std::cin.rdbuf(input.rdbuf()); 
-
-    std::stringstream OutputBuf;
-    std::streambuf* orig_cout = std::cout.rdbuf(OutputBuf.rdbuf());
-
-    MonSelection(Roster, Mon1, Mon2, PlayBot);
-
-    std::cin.rdbuf(orig_cin); 
-    std::cout.rdbuf(orig_cout);
-
-    std::string RetString = OutputBuf.str();
-
+    UserInput = "Y\n5\n4\n7\n";
+    //user input "Y"- bot, 1-3 are valid inputs, rest invalid 
+    std::string RetString = SimulateMonSelection(UserInput); 
     
-    EXPECT_THAT(RetString, testing::HasSubstr("404"));  
-
-    for(auto monster : Roster)
-        delete monster;
+    EXPECT_THAT(RetString, testing::HasSubstr("404"));
 }
 
-TEST(MonsterSelection, MonsterSelection_MaxRetries_Test)
+//No bot - Max Retries limit 
+TEST_F(MonsterFightGame, MonsterFightGame_MaxRetries_Test)
 {
-    std::vector<Monsters*> Roster;
-    Monsters* Mon1 = nullptr; 
-    Monsters* Mon2 = nullptr;
-    bool PlayBot = false;
-
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "Silo"));
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "Kaos")); 
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "IronClaw")); 
-
-    //user input "N"- No bot, 1-3 are valid inputs, rest invalid  
-    std::stringstream input("N\n1\n4\n3\n5\n2\n1\n");
-    std::streambuf* orig_cin = std::cin.rdbuf(input.rdbuf()); 
-
-    std::stringstream OutputBuf;
-    std::streambuf* orig_cout = std::cout.rdbuf(OutputBuf.rdbuf());
-
-    MonSelection(Roster, Mon1, Mon2, PlayBot);
-
-    std::cin.rdbuf(orig_cin); 
-    std::cout.rdbuf(orig_cout);
-
-    std::string RetString = OutputBuf.str();
-
+    UserInput = "N\n1\n4\n3\n5\n2\n1\n";
+    //user input "N"- No bot, 1-3 are valid inputs, rest invalid
+    std::string RetString = SimulateMonSelection(UserInput);  
     
-    EXPECT_THAT(RetString, testing::Not(testing::HasSubstr("404")));  
-
-    for(auto monster : Roster)
-        delete monster;
+    EXPECT_THAT(RetString, testing::Not(testing::HasSubstr("404"))); 
 }
 
-TEST(MonsterSelection, MonsterSelection_MaxRetriesBot_Test)
+//Bot - Max retries limit 
+TEST_F(MonsterFightGame, MonsterFightGame_MaxRetriesBot_Test)
 {
-    std::vector<Monsters*> Roster;
-    Monsters* Mon1 = nullptr; 
-    Monsters* Mon2 = nullptr;
-    bool PlayBot = false;
-
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "Silo"));
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "Kaos")); 
-    Roster.push_back(new Dragons(Monsters::Three_Headed_Dragon, "IronClaw")); 
-
-    //user input "Y"- bot, 1-3 are valid inputs, rest invalid  
-    std::stringstream input("Y\n4\n5\n3\n");
-    std::streambuf* orig_cin = std::cin.rdbuf(input.rdbuf()); 
-
-    std::stringstream OutputBuf;
-    std::streambuf* orig_cout = std::cout.rdbuf(OutputBuf.rdbuf());
-
-    MonSelection(Roster, Mon1, Mon2, PlayBot);
-
-    std::cin.rdbuf(orig_cin); 
-    std::cout.rdbuf(orig_cout);
-
-    std::string RetString = OutputBuf.str();
-
+    UserInput = "Y\n4\n5\n3\n";
+    //user input "Y"- bot, 1-3 are valid inputs, rest invalid
+    std::string RetString = SimulateMonSelection(UserInput);  
     
-    EXPECT_THAT(RetString, testing::Not(testing::HasSubstr("404")));  
-
-    for(auto monster : Roster)
-        delete monster;
+    EXPECT_THAT(RetString, testing::Not(testing::HasSubstr("404")));
 }
 
