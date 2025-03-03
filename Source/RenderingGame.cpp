6bbb34cf51715204af54a5468e73c8b4e6df582d
Samarth Kapoor <samarthkapoor@live.com>
2025-03-03 16:53:11 -0500

    Fixed an issue where game was stuck after choosing champion

--- Source/RenderingGame.cpp
+++ Source/RenderingGame.cpp
@@ -12,40 +12,15 @@ sf::RenderWindow CreateGameWindow()
 
 void UpdateGameWindow(sf::RenderWindow& RndrWindow, Monsters& Mon1, Monsters& Mon2)
 {
-    while(RndrWindow.isOpen())
+    if(RndrWindow.isOpen())
     {
-        while(const std::optional event = RndrWindow.pollEvent())
+        if(const std::optional event = RndrWindow.pollEvent())
         {
             if(event->is<sf::Event::Closed>())
                 RndrWindow.close();
         }
 
         RndrWindow.clear(sf::Color::Black);
-
         RndrWindow.display();
     }    
 }
-
-// int main()
-// {
-//     // sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800,600)), "Monster Battle Arena");
-//     sf::RenderWindow window = CreateGameWindow();
-
-//     std::cout << "Window created successfully" <<std::endl;
-
-//     while(window.isOpen())
-//     {
-//         while(const std::optional event = window.pollEvent())
-//         {
-//             if(event->is<sf::Event::Closed>())
-//                 window.close();
-//         }
-
-//         window.clear(sf::Color::Black);
-
-//         window.display();
-//     }
-
-//     std::cout << "Window closed successfully" << std::endl;
-//     return 0;
-// }
